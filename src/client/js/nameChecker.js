98473fa6df52f827bda14db2ce233cea8972791e
Oluwaseun Alo <oluwaseunalo4@gmail.com>
2021-09-03 20:32:12 +0200

    checking the url validation

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,5 +1,7 @@
-function checkForName(inputText) {
-    console.log("::: Running checkForName :::", inputText);
+function checkForName(url) {
+    console.log("::: Running checkForName :::", url);
+    const regEx = new RegExp(/^(http|https):\/\/[^ "]+$/);
+    return regEx.test(url);
 }
 
 export { checkForName }
